8736afac95fff4aa285ecd17ec8cc1621d4fba1d
pujh <pujh@idste.cn>
2025-07-09 22:25:22 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+#include <stdio.h>
+
+namespace Hazel {
+
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+	}
+	void Application::Run()
+	{
+		while (true);
+	}
+}
